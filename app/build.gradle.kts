5b3114be5667a9cef014b5a8973b2433a3577b77
root <root@localhost>
2026-08-05 21:54:16 +0500

    updated to latest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -28,15 +27,15 @@ android {
                 "proguard-rules.pro"
             )
         }
+        
+        debug {
+            applicationIdSuffix = ".debug"
+        }
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -47,7 +46,7 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.8.0-alpha01")
-    implementation("com.google.android.material:material:1.14.0-beta01")
+    implementation("com.google.android.material:material:1.14.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.github.bumptech.glide:glide:5.0.7")
